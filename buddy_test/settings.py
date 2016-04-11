215fe5caa3af42727e4956736c4d9cdec5768e98
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-04-11 22:39:22 +0200

    Add questionaire app to INCLUDED_APPS

--- buddy_test/settings.py
+++ buddy_test/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'questionaire.apps.QuestionaireConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'buddy_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'buddy',
+        'USER': 'buddy',
+        'PASSWORD': 'buddy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
