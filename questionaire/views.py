b89ef47f823c76f3b431e75d35c4ce0d135368a9
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-04-13 13:13:56 +0200

    Display questionaire list and detail

--- questionaire/views.py
+++ questionaire/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questionaires = Questionaire.objects.order_by('updated_at')
+    context = {
+        'questionaires': questionaires
+    }
+    return render(request, 'questionaire/index.html', context)
+
+def detail(request, questionaire_id):
+    if request.user.is_authenticated():
+        questionaire = get_object_or_404(Questionaire, pk=questionaire_id)
+        return render(request, 'questionaire/index.html',
+        {
+            'questionaire': questionaire
+        })
+    else:
+        return redirect('/login')
